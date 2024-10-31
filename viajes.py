e6e0aad30e4502086efbb9a9170b69004350d06b
nicolasch117 <159283283+nicolasch117@users.noreply.github.com>
2024-10-30 19:08:40 -0500

    Create viajes.py
    se crea la clase viaje

---
+++ viajes.py
@@ -0,0 +1,6 @@
+class Viaje:
+    def __init__(self, origen, destino, fecha, precio):
+        self.origen = origen
+        self.destino = destino
+        self.fecha = fecha
+        self.precio = precio
